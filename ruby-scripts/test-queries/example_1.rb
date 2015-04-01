67a903c67d5e5087c64a76b7622be3f7b5e77706
Cara McCormack <caramcc@ccs.neu.edu>
2015-03-31 23:52:44 -0400

    start writing SQL tests

---
+++ ruby-scripts/test-queries/example_1.rb
@@ -0,0 +1,36 @@
+require 'net/http'
+require 'json'
+# require 'active_support'
+require 'mysql2'
+
+@table = 'caramcc_tv_shows'
+
+# def load_data
+#   all_data = []
+#   Dir.entries('output-data2').drop(2).each do |file_path|
+#     File.open(File.join('output-data2', file_path), "r") do |f|
+#       f.each_line do |line|
+#         all_data.push JSON.parse(line)
+#       end
+#     end
+#   end
+#   all_data
+# end
+
+@client = Mysql2::Client.new(:host => "localhost", :username => "root")
+
+@client.query("USE caramcc_dbd_project")
+
+# result = @client.query("SELECT * FROM #{@table}")
+
+# result.each do |row|
+#   puts row["show_title"]
+# end
+
+actor = ARGV[0]
+
+result = @client.query("SELECT * FROM #{@table} WHERE actors LIKE '%#{actor}%';")
+
+result.each do |row|
+  puts row['show_title']
+end
