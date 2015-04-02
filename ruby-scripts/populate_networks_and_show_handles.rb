3333765e4c380fa989fbf07f4615081bff6d7537
Cara McCormack <caramcc@ccs.neu.edu>
2015-04-01 23:34:55 -0400

    work dumber, but actually work re: populating networks

--- ruby-scripts/populate_networks_and_show_handles.rb
+++ ruby-scripts/populate_networks_and_show_handles.rb
@@ -6,7 +6,12 @@ require_relative 'constants.rb'
 @client = Mysql2::Client.new(:host => $host, :username => $username, :password => $password)
 @client.query("USE #{$db_name}")
 
-@client.query("SELECT DISTINCT network FROM #{$tv_shows};").each do |network|
+networks = []
+@client.query("SELECT network FROM #{$tv_shows};").each do |network|
+  networks.push network['network']
+end
+
+networks.each do |network|
   unless network['network_name'].nil?
     @client.query("INSERT INTO #{$networks} (network_name) VALUES ('#{network['network_name']}');")
   end
@@ -20,6 +25,6 @@ twitter_handle_data['show_data'].each do |show, handle|
   @client.query("UPDATE #{$tv_shows} SET show_twitter_id = '#{handle['twitter_handle']}' WHERE show_title LIKE '#{@client.escape(show)}';")
 end
 
-twitter_handle_data["network_data"].each do |network, handle|
-  @client.query("UPDATE #{$networks} SET network_twitter_id = '#{handle['twitter_handle']}' WHERE network_name LIKE '#{@client.escape(network)}';")
+twitter_handle_data['network_data'].each do |network, handle|
+  @client.query("INSERT INTO #{$networks}  (network_name, network_twitter_id) VALUES ('network', '#{handle['twitter_handle']}');")
 end
