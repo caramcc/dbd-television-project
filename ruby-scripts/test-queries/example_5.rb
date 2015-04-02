d6f0a59e2e3eec38d2fc66ca718d692d76a2a1f8
Cara McCormack <caramcc@ccs.neu.edu>
2015-04-01 23:30:19 -0400

    ex5

--- ruby-scripts/test-queries/example_5.rb
+++ ruby-scripts/test-queries/example_5.rb
@@ -13,7 +13,12 @@ require_relative '../constants.rb'
 
 creator = 'J.J. Abrams'
 
-result = @client.query("SELECT * FROM #{$tv_shows} WHERE creators LIKE '%#{creator}%';")
+result = @client.query("SELECT c.*, s.start_date, s.end_date, s.show_title, DATEDIFF(s.start_date, s.end_date )
+FROM #{$tv_shows} s
+JOIN #{$show_creators} sc ON sc.show_id = s.show_id
+JOIN #{$creators} c ON c.creator_id = sc.creator_id
+WHERE creator_name LIKE '%#{creator}%'
+ORDER BY DATEDIFF(s.start_date, s.end_date ) ASC;")
 
 result.each do |row|
   puts row['show_title']
