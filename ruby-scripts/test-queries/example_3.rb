71992d279f1d9a3b287199f5a288fbf6e88563ed
Cara McCormack <caramcc@ccs.neu.edu>
2015-04-11 19:39:58 -0400

    example... 3...?

--- ruby-scripts/test-queries/example_3.rb
+++ ruby-scripts/test-queries/example_3.rb
@@ -13,13 +13,17 @@ require_relative '../constants.rb'
 
 actor = 'Hugh Laurie'
 
-result = @client.query("SELECT sg.*, a.*, s.show_id, COUNT(sg.genre)
-FROM #{$actors} a
-JOIN #{$show_actors} sa ON a.actor_id = sa.actor_id
-JOIN #{$tv_shows} s ON sa.show_id = s.show_id
-JOIN #{$show_genres} sg ON s.show_id = sg.show_id
-WHERE a.actor_name LIKE '#{actor}'")
+@client.query("CREATE OR REPLACE VIEW caramcc_actor_genre_frequency
+  AS
+  SELECT sg.*, a.*, s.show_id AS sid, MAX(sg.genre)
+  FROM #{$actors} a
+  JOIN #{$show_actors} sa ON a.actor_id = sa.actor_id
+  JOIN #{$tv_shows} s ON sa.show_id = sid
+  JOIN #{$show_genres} sg ON sid = sg.show_id")
+
+result = @client.query("SELECT MAX(genre) FROM caramcc_actor_genre_frequency
+WHERE actor_name LIKE '#{actor}'")
 
 result.each do |row|
-  puts row['genre']
+  puts row['MAX(genre)']
 end
